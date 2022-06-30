8dffee3d820c0e15d87d19da002b557c8fbae6a3
Peter Pakanun <lpeachl@gmail.com>
2022-06-30 11:04:55 +0700

    fix

--- rollup.config.js
+++ rollup.config.js
@@ -5,14 +5,14 @@ import css from "rollup-plugin-css-only";
 const pkg = require('./package.json');
 
 export default {
-  input: 'src/Icon.svelte',
+  input: 'src/Index.svelte',
   output: [
     { file: pkg.module, format: "es" },
     { file: pkg.main, format: "umd", name: "Icon" },
   ],
   plugins: [
     svelte(),
-    css({ output: 'icons.css' }),
+    css({ output: 'index.css' }),
     resolve()
   ],
 };
