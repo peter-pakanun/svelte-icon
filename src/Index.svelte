2eccfb1695ebe95ac557e3d8a0cc23fe357782b8
Peter Pakanun <lpeachl@gmail.com>
2022-07-01 22:18:19 +0700

    replace tab with spaces

--- src/Index.svelte
+++ src/Index.svelte
@@ -4,8 +4,8 @@
   export let stroke;
   export let title;
 
-	let classProp;
-	export { classProp as class };
+  let classProp;
+  export { classProp as class };
 </script>
 
 <svg viewBox="0 0 24 24" class={classProp} {...$$restProps}>
